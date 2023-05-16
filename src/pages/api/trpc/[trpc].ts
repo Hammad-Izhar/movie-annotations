65666fb7274fb16b385deca90ff770f9f0094b16
Hammad Izhar <hammad_izhar@brown.edu>
2023-05-16 14:53:04 -0400

    fix merge conflicts

---
+++ src/pages/api/trpc/[trpc].ts
@@ -0,0 +1,19 @@
+import { createNextApiHandler } from "@trpc/server/adapters/next";
+
+import { env } from "@movies/env.mjs";
+import { createTRPCContext } from "@movies/server/api/trpc";
+import { appRouter } from "@movies/server/api/root";
+
+// export API handler
+export default createNextApiHandler({
+  router: appRouter,
+  createContext: createTRPCContext,
+  onError:
+    env.NODE_ENV === "development"
+      ? ({ path, error }) => {
+          console.error(
+            `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
+          );
+        }
+      : undefined,
+});
