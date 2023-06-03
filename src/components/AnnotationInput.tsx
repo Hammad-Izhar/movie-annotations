241ea99159a3c96033f698dabb8b613133e46500
Hammad Izhar <hammad_izhar@brown.edu>
2023-06-03 15:28:04 -0400

    got slider input working

--- src/components/AnnotationInput.tsx
+++ src/components/AnnotationInput.tsx
@@ -1,12 +1,70 @@
+import clsx from "clsx";
+import { useEffect, useState } from "react";
+
+type Rating = undefined | 1 | 2 | 3 | 4 | 5;
+
 const AnnotationInput = () => {
+  const [selectedRating, setSelectedRating] = useState<Rating>(undefined);
+
   return (
-    <div className="flex h-32">
-      <div className="basis-full bg-yellow-500"></div>
-      <div className="basis-full bg-green-500"></div>
-      <div className="basis-full bg-blue-500"></div>
-      <div className="basis-full bg-indigo-500"></div>
-      <div className="basis-full bg-violet-500"></div>
-    </div>
+    <>
+      <div className="relative flex h-32 items-center">
+        <div
+          className={clsx(
+            "grid h-full basis-full place-items-center bg-yellow-500",
+            selectedRating == 1 ? "ring ring-inset ring-red-500" : ""
+          )}
+        >
+          1
+        </div>
+        <div
+          className={clsx(
+            "grid h-full basis-full place-items-center bg-green-500",
+            selectedRating == 2 ? "ring ring-inset ring-red-500" : ""
+          )}
+        >
+          2
+        </div>
+        <div
+          className={clsx(
+            "grid h-full basis-full place-items-center bg-blue-500",
+            selectedRating == 3 ? "ring ring-inset ring-red-500" : ""
+          )}
+        >
+          3
+        </div>
+        <div
+          className={clsx(
+            "grid h-full basis-full place-items-center bg-indigo-500",
+            selectedRating == 4 ? "ring ring-inset ring-red-500" : ""
+          )}
+        >
+          4
+        </div>
+        <div
+          className={clsx(
+            "grid h-full basis-full place-items-center bg-violet-500",
+            selectedRating == 5 ? "ring ring-inset ring-red-500" : ""
+          )}
+        >
+          5
+        </div>
+        <input
+          type="range"
+          className="absolute h-full w-full"
+          onPointerUp={() => setSelectedRating(undefined)}
+          onTouchEnd={() => setSelectedRating(undefined)}
+          onChange={(e) => {
+            const section = Math.floor(e.target.valueAsNumber / 20) + 1;
+
+            if (section < 1) setSelectedRating(1);
+            else if (section > 5) setSelectedRating(5);
+            else setSelectedRating(section as Rating);
+          }}
+        />
+      </div>
+      <span>Selected Rating: {selectedRating ?? "No Rating"}</span>
+    </>
   );
 };
 
